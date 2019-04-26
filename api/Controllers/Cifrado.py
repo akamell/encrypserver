5e89de36a34c1750acae44c1cede165b1703318b
Abraham Kamell <kamela@uninorte.edu.co>
2019-04-26 00:09:54 -0500

    AES cifrar texto

--- api/Controllers/Cifrado.py
+++ api/Controllers/Cifrado.py
@@ -8,7 +8,7 @@ from api.Helpers.Aes import Aes
 
 mod = Blueprint('CifradoController',__name__)
 
-@mod.route("archivo", methods=['POST'])
+@mod.route("", methods=['POST'])
 def cifrarArchivo():
     jsondata = Helper.validarDataPost(request)	
     if jsondata == None:
@@ -26,7 +26,31 @@ def cifrarArchivo():
     if passwd == '':
         return Helper.jsonResponse(-4, "Error parametro no valido", None)
     
-    aes = Aes("P1ssw0rd2")
+    aes = Aes(passwd)
     resultado = aes.encrypt(archivo)
+    
+    return Helper.jsonResponse(200, "Cifrado", resultado)
+
+
+@mod.route("", methods=['PUT'])
+def descifrarArchivo():
+    jsondata = Helper.validarDataPost(request)	
+    if jsondata == None:
+	    return Helper.jsonResponse(400, "Json invalid "+str(jsondata), None)
+    ip = request.remote_addr
+    if 'archivo' in jsondata is False:
+	    return Helper.jsonResponse(-1, "username no puede ser vacio", None)
+    if 'password' in jsondata is False:
+	    return Helper.jsonResponse(-2, "Password no puede ser vacio", None)
+
+    archivo = jsondata.get('archivo','')
+    if archivo == '':
+        return Helper.jsonResponse(-3, "Error parametro no valido", None)
+    passwd = jsondata.get('password','')
+    if passwd == '':
+        return Helper.jsonResponse(-4, "Error parametro no valido", None)
+    
+    aes = Aes("P1ssw0rd2")
+    resultado = aes.decrypt(archivo)
     
     return Helper.jsonResponse(200, "Cifrado", resultado)
--- run.py
+++ run.py
@@ -1,2 +1,3 @@
+#!/usr/bin/env python3
 from api import app
-app.run(host='0.0.0.0', port='5000')
+app.run(host='0.0.0.0', port='5000', debug=True)
